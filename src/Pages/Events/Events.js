08d2892dffa9eb3108ae02d33043c09b86e2447f
Dhanashree <dvphalak@gmail.com>
2019-01-15 19:29:52 -0500

    savin work

--- src/Pages/Events/Events.js
+++ src/Pages/Events/Events.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import API from '../../utils/API';
 
 class Events extends Component {
-  state = {};
+  state = {
+    Events: []
+  };
+  componentDidMount() {
+    this.getEvents();
+  }
+
+  getEvents = () => {
+    API.getSavedEvents()
+      .then(({data}) => this.setState({events: data}))
+      .catch(err => console.log(err));
+  }
   render() {
-    return <div />;
+    return(
+      
+      <div class="eventlist">
+      {
+        this.state.Events.map(event => {
+          return(
+            <div className= "event" key={Event.id}>
+              <h3 className="heading">
+                {Event.eventName}
+              </h3>
+              <h5>
+                {Event.date}
+              </h5>
+              <h5>{Event.locationName}</h5>
+            </div>
+          )
+        })
+      }
+      </div>
+
+    )
   }
 }
 
